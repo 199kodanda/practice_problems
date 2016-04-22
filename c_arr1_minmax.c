adc63ea259438e520ed716d9df02b93f620263fe
kodandaram199 <kodandaram199@gmail.com>
2016-04-22 20:43:38 +0530

    C Arrays Practice 1 added

---
+++ c_arr1_minmax.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+void fun(int *a,int n,int *arr)
+{
+	int i,min=a[0],max=a[0];
+	for(i=1;i<n;i++)
+	{
+		if(max<a[i])
+			max=a[i];
+		if(min>a[i])
+			min=a[i];
+	}
+	arr[0]=max;
+	arr[1]=min;
+}
+void main()
+{
+	int n,*a,i;
+	printf("Enter Array Size: ");
+	scanf("%d",&n);
+	a=(int *)malloc(n*sizeof(int));
+	printf("Enter Array: ");
+	for(i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	fun(a,n);
+	printf("Max: %d\n",a[0]);
+	if(a[0]==a[1])
+		printf("Max and Min are same..!");
+	else
+		printf("Min: %d ",a[1]);
+	getch();
+}
